37ace42f3eacebc10b6f003f742770ac81315a1a
Philipp <philipp.lackner@yahoo.de>
2020-07-22 23:28:15 +0200

    This is our second commit

--- app/src/main/java/com/androiddevs/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/androiddevs/gitpracticeyt/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("THIS IS CONTAINED IN MY SECOND COMMIT")
     }
 }
