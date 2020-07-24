732b433db4f5f26545997cec0ba573a2f9c3a143
Philipp <mike.litoris@gmail.com>
2020-07-24 22:52:31 +0200

    This is our third commit

--- app/src/main/java/com/androiddevs/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/androiddevs/gitpracticeyt/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("THIS IS CONTAINED IN MY SECOND COMMIT")
+        today is a good day
     }
 }
