190892cfd5a9822af0a298c4487d58598b2ed34d
Philipp <mike.litoris@gmail.com>
2020-07-26 00:01:13 +0200

    Experimental Commit

--- app/src/main/java/com/androiddevs/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/androiddevs/gitpracticeyt/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("THIS IS CONTAINED IN MY SECOND COMMIT")
+        println("LOCAL CHANGE")
+        println("Experimental")
     }
 }
