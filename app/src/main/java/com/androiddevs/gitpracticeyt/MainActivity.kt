5560902317e49dfc7f76e83e4cee6791e219eae0
Philipp <mike.litoris@gmail.com>
2020-07-28 14:11:28 +0200

    final merge change 2

--- app/src/main/java/com/androiddevs/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/androiddevs/gitpracticeyt/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
         println("THIS IS CONTAINED IN MY SECOND COMMIT")
         println("LOCAL CHANGE")
-        println("CHANGE 2")
+        println("CHANGE 4")
         println("THIS LINE WILL BE MERGED")
     }
 }
